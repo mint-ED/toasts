be1c9a0c6026fc66e372ffb8331b62f236e86b84
Jon Saginaw <saginawj@gmail.com>
2022-03-01 06:53:00 -0500

    add TotalSupply interface and functionality

--- contracts/TOAST/TOAST2.sol
+++ contracts/TOAST/TOAST2.sol
@@ -991,7 +991,65 @@ contract ERC1155 is Context, ERC165, IERC1155, IERC1155MetadataURI {
     }
 }
 
-// File: mintED-TOAST.sol
+// File: @openzeppelin/contracts/token/ERC1155/extensions/ERC1155Supply.sol
+
+
+// OpenZeppelin Contracts v4.4.1 (token/ERC1155/extensions/ERC1155Supply.sol)
+
+pragma solidity ^0.8.0;
+
+
+/**
+ * @dev Extension of ERC1155 that adds tracking of total supply per id.
+ *
+ * Useful for scenarios where Fungible and Non-fungible tokens have to be
+ * clearly identified. Note: While a totalSupply of 1 might mean the
+ * corresponding is an NFT, there is no guarantees that no other token with the
+ * same id are not going to be minted.
+ */
+abstract contract ERC1155Supply is ERC1155 {
+    mapping(uint256 => uint256) private _totalSupply;
+
+    /**
+     * @dev Total amount of tokens in with a given id.
+     */
+    function totalSupply(uint256 id) public view virtual returns (uint256) {
+        return _totalSupply[id];
+    }
+
+    /**
+     * @dev Indicates whether any token exist with a given id, or not.
+     */
+    function exists(uint256 id) public view virtual returns (bool) {
+        return ERC1155Supply.totalSupply(id) > 0;
+    }
+
+    /**
+     * @dev See {ERC1155-_beforeTokenTransfer}.
+     */
+    function _beforeTokenTransfer(
+        address operator,
+        address from,
+        address to,
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal virtual override {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+
+        if (from == address(0)) {
+            for (uint256 i = 0; i < ids.length; ++i) {
+                _totalSupply[ids[i]] += amounts[i];
+            }
+        }
+
+        if (to == address(0)) {
+            for (uint256 i = 0; i < ids.length; ++i) {
+                _totalSupply[ids[i]] -= amounts[i];
+            }
+        }
+    }
+}
 
 pragma solidity ^0.8.0;
 
@@ -1017,7 +1075,7 @@ abstract contract Ownable {
     }
 }
 
-contract TOAST is ERC1155, Ownable {
+contract TOAST is ERC1155, ERC1155Supply, Ownable {
     constructor() ERC1155("") {}
 
     //Supply
@@ -1028,6 +1086,10 @@ contract TOAST is ERC1155, Ownable {
 
     function getTokenMaxSupply(uint256 tokenId_)  external view onlyOwner returns(uint256) {
         return tokenIdtoTokenMaxSupply[tokenId_]; }
+
+    function getTokenSupply(uint256 tokenId_) external view onlyOwner returns(uint256){
+        return totalSupply(tokenId_); }
+
 
 
     // internal function to build urls
@@ -1106,4 +1168,27 @@ contract TOAST is ERC1155, Ownable {
             "ERC1155: caller is not owner nor approved");
         _burnBatch(account, ids, values);
     }
+
+
+     // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address operator, address from, address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        internal
+        override(ERC1155, ERC1155Supply)
+    {
+        super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
+    }
+
+    /**
+     * @dev See {IERC165-supportsInterface}.
+     */
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC1155)
+        returns (bool)
+    {
+        return interfaceId == type(IERC1155).interfaceId || super.supportsInterface(interfaceId);
+    }
+
 }
