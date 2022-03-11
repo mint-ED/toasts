409a5322b7b0f6c5f264b6d63a483f4786d76461
Jon Saginaw <saginawj@gmail.com>
2022-03-11 12:41:00 -0500

    updates to core deployment script to use ethers

--- backend/scripts/toast-deploy.js
+++ backend/scripts/toast-deploy.js
@@ -1,29 +1,19 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+const { ethers } = require("hardhat");
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
+  
+  const [deployer] = await ethers.getSigners();
 
-  // We get the contract to deploy
-  const Toast = await hre.ethers.getContractFactory("contracts/TOAST.sol:TOASTS");
+  console.log("Deploying contracts with this account: ", deployer.address);
+  console.log("Account balance: ", (await deployer.getBalance()).toString());
+
+  const Toast = await ethers.getContractFactory("contracts/TOAST.sol:TOASTS");
   const toast = await Toast.deploy();
-
   await toast.deployed();
 
   console.log("Toast deployed to:", toast.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main()
   .then(() => process.exit(0))
   .catch((error) => {
