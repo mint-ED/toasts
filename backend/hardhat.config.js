7e5d6f631190da0c306cf50b7de48406a6275f26
Jon Saginaw <saginawj@gmail.com>
2022-03-11 14:10:44 -0500

    hardhat deployment script to mumbai via alchemy

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -3,15 +3,24 @@ require("@nomiclabs/hardhat-ethers");
 //require('@openzeppelin/hardhat-upgrades');
 require('dotenv').config()
 
+const MUMBAI_ALCHEMY_URL  = process.env.MUMBAI_ALCHEMY_URL;
+const MUMBAI_DEV_KEY      = process.env.MUMBAI_DEV_KEY;
+const GANACHE_URL         = process.env.GANACHE_URL;
+//const GANACHE_URL       = "http://127.0.0.1:7545"
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
  module.exports = {
-  defaultNetwork: "ganache",
+  //defaultNetwork: "ganache",
   networks: {
       ganache: {
-          url: "http://127.0.0.1:7545",
+          url: `${GANACHE_URL}`,
           // accounts: [privateKey1, privateKey2, ...]
+      },
+      mumbai: {
+        url: `${MUMBAI_ALCHEMY_URL}`,
+        accounts: [`${MUMBAI_DEV_KEY}`]
       }
   },
   solidity: {
