1e3c2de8692dfca88a83f69d7b6aa6b527648b36
Jon Saginaw <saginawj@gmail.com>
2022-03-11 11:25:19 -0500

    basic toast deployment script

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -2,11 +2,6 @@ require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
 require('dotenv').config()
 
-console.log("Verifying the TEST_URL is convigured via dotenv", process.env.TEST_URL);
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
