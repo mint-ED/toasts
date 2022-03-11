35ddeb07d9d760f7555ca05674f6b0beb44715e1
Jon Saginaw <saginawj@gmail.com>
2022-03-11 11:36:33 -0500

    deploy to ganache by default (update to hardhat config)

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,11 +1,19 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-ethers");
+//require('@openzeppelin/hardhat-upgrades');
 require('dotenv').config()
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
  module.exports = {
+  defaultNetwork: "ganache",
+  networks: {
+      ganache: {
+          url: "http://127.0.0.1:7545",
+          // accounts: [privateKey1, privateKey2, ...]
+      }
+  },
   solidity: {
     version: "0.8.4",
     settings: {
