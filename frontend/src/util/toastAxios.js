942985db99fe9a184ba87fc61efbd490a22aac74
Jon Saginaw <saginawj@gmail.com>
2022-03-12 09:39:56 -0500

    call to getNFTs with wallet/contract works, but need to fix dotenv

--- frontend/src/util/toastAxios.js
+++ frontend/src/util/toastAxios.js
@@ -1,11 +1,17 @@
+//require('dotenv').config({path:__dirname + '../../.env'});
+
 require('dotenv').config();
 const axios = require('axios');
 
 // Replace with your Alchemy API key:
-const apiKey = "demo";
-const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${apiKey}/getNFTs/`;
+//const apiKey = "demo";
+//const baseURL = `https://eth-mainnet.alchemyapi.io/v2/${apiKey}/getNFTs/`;
+const polygonMainnetURL = process.env.POLYGON_MAINNET_URL;
+console.log("url i'm getting:", polygonMainnetURL);
+const baseURL = `${polygonMainnetURL}/getNFTs/`;
 // Replace with the wallet address you want to query for NFTs:
-const ownerAddr = "0xF5FFF32CF83A1A614e15F25Ce55B0c0A6b5F8F2c";
+//const ownerAddr = "0xF5FFF32CF83A1A614e15F25Ce55B0c0A6b5F8F2c";
+const contractAddresses = ["0xa4A04947869D8201da08e5d9abfF0c5bA78689C5"];
 
 
 
@@ -17,7 +23,7 @@ export const getNftsAxios = async(textboxWalletAddress) => {
     // Construct the axios request:
     var config = {
         method: 'get',
-        url: `${baseURL}?owner=${textboxWalletAddress}`
+        url: `${baseURL}?owner=${textboxWalletAddress}&contractAddresses[]=${contractAddresses}`
     };
 
     // TODO: use the wallet address passed in
