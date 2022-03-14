b1587e64751285ce704c6f156766f7c999807264
Jon Saginaw <saginawj@gmail.com>
2022-03-14 10:52:16 -0400

    additional exchange validation test scripts added and checked

--- backend/test/remix/test-remix.sol
+++ backend/test/remix/test-remix.sol
@@ -7,17 +7,32 @@ import "../contracts/TOAST.sol";
 
 contract ToastTest2 {
    
-   
     TOASTS toast;
     address ownerAddr;
-    address addr2;
+    address alice;
+    address bob;
+    uint256 perfectAttendanceTokenId;
+    uint256[] dailyAttendanceTokenIds;    
 
     function beforeAll () public {
+        //contract
         toast = new TOASTS();
+        //addresses
         ownerAddr = TestsAccounts.getAccount(0);
-        addr2 = TestsAccounts.getAccount(1);
+        alice = TestsAccounts.getAccount(1);
+        bob = TestsAccounts.getAccount(2);
+
+        //tokens
+        perfectAttendanceTokenId = 6;
+
+        dailyAttendanceTokenIds = new uint256[](4);
+        dailyAttendanceTokenIds[0] = 1;
+        dailyAttendanceTokenIds[1] = 2;
+        dailyAttendanceTokenIds[2] = 3;
+        dailyAttendanceTokenIds[3] = 4;
+
     }
-    
+   
     function checkContractNameAndSymbol () public {
         Assert.equal(toast.name(), "mintED Toasts", "initial contract should be named mintED Toasts");
         
@@ -37,8 +52,42 @@ contract ToastTest2 {
         toast.toastSingleToSingle(ownerAddr, tokenId, amount, data);
 
         Assert.equal(toast.balanceOf(ownerAddr, tokenId), amount, "balanceOf account's tokenId should be amount");
-
     }
-    
+
+    function checkTokenExchangeQualification () public {
+
+        bytes memory data;
+        
+        toast.setNewToBurnableMapping(perfectAttendanceTokenId, dailyAttendanceTokenIds);
+
+        //Alice gets all 4 tokens   
+        toast.toastSingleToSingle(alice, 1, 1, data);
+        toast.toastSingleToSingle(alice, 2, 1, data);
+        toast.toastSingleToSingle(alice, 3, 1, data);
+        toast.toastSingleToSingle(alice, 4, 1, data);
+
+        //bob only gets 3 of the required 4 tokens  
+        toast.toastSingleToSingle(bob, 1, 1, data);
+        toast.toastSingleToSingle(bob, 2, 1, data);
+        toast.toastSingleToSingle(bob, 3, 1, data);
+        toast.toastSingleToSingle(bob, 10, 1, data);
+
+        bool aliceQuals = toast.checkExchangeQualification(alice, perfectAttendanceTokenId);
+        bool bobQuals = toast.checkExchangeQualification(bob, perfectAttendanceTokenId);
+
+        //alice should qualify bc she has the 4 required daily tokens  
+        Assert.equal(aliceQuals, true, "alice should qualify but didn't");
+         //bob should not qualify bc he does not have the 4 required daily tokens  
+        Assert.equal(bobQuals, false, "bob shouldn't qualify but did");
+
+        //give bob the last required token and check if he's qualified
+        toast.toastSingleToSingle(bob, 4, 1, data);
+        bobQuals = toast.checkExchangeQualification(bob, perfectAttendanceTokenId);
+
+        //bob should now qualify 
+        Assert.equal(bobQuals, true, "bob got the 4th and should qualify but doesn't");
+    }
+
+  
 
 }
